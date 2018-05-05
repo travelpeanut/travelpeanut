74aa271bd854ea957c931c0ac9fc6e461dc6a79a
Ryan Schabel <schabel12@gmail.com>
2018-05-05 12:37:41 -0400

    working on adding discover places, getting coordinates for city with axios req

--- client/src/actions/discoveryActions.js
+++ client/src/actions/discoveryActions.js
@@ -1,3 +1,29 @@
 import ActionTypes from '../constants/ActionTypes'
 import { push } from 'react-router-redux'
 import axios from 'axios'
+
+const getCoordinatesByCity = (cityAndState) => (dispatch, getState) => {
+    console.log('in discoveryActions: ', cityAndState)
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${cityAndState}&key=AIzaSyB0viycMhEqrmrdp841mv_wGEkHNGCrk_s`)
+    // .then((success))
+    // dispatch({
+    //     type: ActionTypes.STORE_SOMETHING,
+    //     code: success
+    // })
+    // .catch(err => console.log('couldnt set coordinates:', err))
+    // https://maps.googleapis.com/maps/api/geocode/json?address=MountainView,+CA&key=AIzaSyB0viycMhEqrmrdp841mv_wGEkHNGCrk_s
+    //I don't think we need to dispatch push because we just want to update state
+    //dispatch(push(`/trip/${}))
+
+}
+
+const createTrip = (data) => (dispatch, getState) => {
+    console.log('in actions:', data)
+    dispatch({
+      type: ActionTypes.CREATE_TRIP,
+      code: data
+    }) 
+  
+    dispatch(push(`/trip/${data.name}`));
+  
+  }
--- client/src/reducers/tripReducer.js
+++ client/src/reducers/tripReducer.js
@@ -1,12 +1,14 @@
 import ActionTypes from '../constants/ActionTypes'
 import { updateObject, createReducer } from'./reducerUtils'
 
+// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
 
 const initialState = {
   allTrips: [],
   currentTrip: {},
   currentDay: {},
   temp: {},
+  tripCoordinates: ''
 }
 
 const tripReducer = (state = initialState, action) => {
