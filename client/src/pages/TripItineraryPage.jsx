04e5b88ad3afa68da4d6c5a0000e821c426401d4
Heidi Poon <heidixpoon@gmail.com>
2018-05-08 15:59:18 -0400

    dates showing in itinerary summary page

--- client/src/pages/TripItineraryPage.jsx
+++ client/src/pages/TripItineraryPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import * as tripActions from '../actions/tripActions.js';
-
+import * as tripActions from '../actions/tripActions.js'
+import moment from 'moment'
 
 class TripItinerary extends React.Component {
   constructor(props) {
@@ -19,17 +19,35 @@ class TripItinerary extends React.Component {
   
   render() {
     let {currentTrip} = this.props.tripState
+    let start = new Date(currentTrip.start_date)
+    let end = new Date(currentTrip.end_date)
+    var dayCount = Math.round(Math.abs((end.getTime() - start.getTime())/(24*60*60*1000))) + 1
+    let dayArr = [];
+    for(var i=1; i<=dayCount; i++){
+      let day = moment(start).day(i-1,'days')._d;
+      let date = moment(day).date()
+      let m = moment(day).month()+1
+      let year = moment(day).year()
+      dayArr.push([i, m, date, year]);
+    }
 
     return(
       <div>
        <h1>This is Trip: {currentTrip.title}</h1>
 
-       <p>day 1</p>
-       <button onClick={this.redirectToDetails}>View Details</button>
+      {
+        dayArr.map((item, i) => {
+          return(
+            <div key={i}>
+              <p>day {item[0]} </p>
+              <p>{item[1]}/{item[2]}/{item[3]} </p>
+              <button onClick={this.redirectToDetails}>View Details</button>
+            </div>
+          )
+        })
+      }
 
-       <p>day 2 ..</p>
-       <button onClick={this.redirectToDetails}>View Details</button>
-
+    
        
       </div>
     )
