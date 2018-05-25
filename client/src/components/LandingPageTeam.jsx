30fd94e1cdccd8486ddba33da5743537ea2b3467
David <davidbaek92@gmail.com>
2018-05-24 22:46:48 -0400

    Working on landing page

--- client/src/components/LandingPageTeam.jsx
+++ client/src/components/LandingPageTeam.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import github from '../styles/img/github.png'
 
 const LandingPageTeam = () => {
  return (
@@ -15,70 +16,76 @@ const LandingPageTeam = () => {
       {/* Heidi */}
       <div className="team">
         <div className="team-image flip-container">
-          <div className="front">
+          <div className="team-container textWithBlurredBg">
+            <div className="team-info">
+              Corgies, Samyoed, and LINE stickers
+            </div>
             <img src="https://avatars0.githubusercontent.com/u/12095864?s=460&v=4"/>          
-          </div>
-          <div className="back">
-            <img src="https://avatars1.githubusercontent.com/u/29075711?s=400&u=b9f08e4198042d7afc21212413241a9bce58a390&v=4"/>
-          </div>
-        </div>
-
-        <div className="team-info">
-
-        </div>
+          </div>          
+        </div>        
 
         <div className="team-name">          
           <h4>Heidi Poon</h4>
           <div>Product Lead | Front-End Developer</div>
+          <a className="github" href="https://github.com/heidixpoon" target="_blank"><img src={github}/></a>
         </div>
       </div>
 
       {/* David */}
       <div className="team">
-        <div className="team-image">
-          <img src="https://avatars1.githubusercontent.com/u/29075711?s=400&u=b9f08e4198042d7afc21212413241a9bce58a390&v=4"/>          
-        </div>
+        <div className="team-image flip-container">
+          <div className="team-container textWithBlurredBg">
+            <div className="team-info">
 
-        <div className="team-info">
-
+            </div>
+            <img src="https://avatars1.githubusercontent.com/u/29075711?s=400&u=b9f08e4198042d7afc21212413241a9bce58a390&v=4"/>          
+          </div>          
         </div>
+        
 
         <div className="team-name">          
           <h4>David Baek</h4>
           <div>Scrum Master | Back-End Developer</div>
+          <a className="github" href="https://github.com/davidbaek92" target="_blank"><img src={github}/></a>
         </div>
 
       </div>
 
       {/* Brian */}
       <div className="team">
-        <div className="team-image">
-          <img src="https://avatars3.githubusercontent.com/u/35786667?s=460&v=4"/>          
-        </div>
+        <div className="team-image flip-container">
+          <div className="team-container textWithBlurredBg">
+            <div className="team-info">
 
-        <div className="team-info">
-
+            </div>
+            <img src="https://avatars3.githubusercontent.com/u/35786667?s=460&v=4"/>          
+          </div>          
         </div>
+        
 
         <div className="team-name">          
           <h4>Brian Fang</h4>
           <div>Back-End Developer</div>
+          <a className="github" href="https://github.com/bfang212" target="_blank"><img src={github}/></a>
         </div>
       </div>
       
       {/* Ryan */}
       <div className="team">
-        <div className="team-image">
-          <img src="https://avatars1.githubusercontent.com/u/34013422?s=460&v=4"/>          
-        </div>
+        <div className="team-image flip-container">
+          <div className="team-container textWithBlurredBg">
+            <div className="team-info">
 
-        <div className="team-info">
-
+            </div>
+            <img src="https://avatars1.githubusercontent.com/u/34013422?s=460&v=4"/>          
+          </div>          
         </div>
+        
 
         <div className="team-name">          
           <h4>Ryan Schabel</h4>
           <div>Front-End Developer</div>
+          <a className="github" href="https://github.com/schabel12" target="_blank"><img src={github}/></a>
         </div>
       </div>
 
